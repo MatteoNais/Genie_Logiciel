8713302e0d9220136e70b94973f7ff73777bb867
naism <naism@MSI>
2023-01-18 20:25:19 +0100

    Ajout test dossier bancaire avec classe Compte_courant

--- JavaProgSujet/src/tests/TestDossierBancaire.java
+++ JavaProgSujet/src/tests/TestDossierBancaire.java
@@ -9,12 +9,16 @@ import myPackage.DossierBancaire;
 public class TestDossierBancaire {
 
 	@Test
-	public void test() {
+	public void test_constructeur() {
 		DossierBancaire db = new DossierBancaire();
 		
 		assertEquals(0, db.get_solde(),0);
+	}
+	@Test
+	public void test_depot() {
+		DossierBancaire db = new DossierBancaire();
 		db.deposer(20);
-		assertEquals(20, db.get_solde(), 0);
+		assertEquals(20, db.get_solde(),0);
 	}
 
 }
