d9ee842a189a7a104a4c58298a3910bc8fa8d998
naism <naism@172.18.21.157>
2023-01-19 14:06:11 +0100

    Documentation classe dossier bancaire

--- JavaProgSujet/src/myPackage/DossierBancaire.java
+++ JavaProgSujet/src/myPackage/DossierBancaire.java
@@ -2,27 +2,27 @@ package myPackage;
 
 public class DossierBancaire {
 	
-	//Constructeur
-	private Compte_courant cc;
-	private Compte_epargne ce;
-    public DossierBancaire()
+	
+	private Compte_courant cc; // Création d'un objet compte courant .
+	private Compte_epargne ce; // Création d'un objet compte épargne.
+	public DossierBancaire() //Constructeur
     {
-    	cc = new Compte_courant();
+    	cc = new Compte_courant(); //Initialisation des objets compte courant et compte épargne.
     	ce = new Compte_epargne();
     }
 
-    public void deposer(double value) 
+    public void deposer(double value) // méthode permettant de déposer de l'argent.
     {
     	cc.add_solde(value*0.4);
     	ce.add_solde(value*0.6);
     }
-    public double get_solde() 
+    public double get_solde() // méthode permettant de consulter le solde.
     {
     	double total = 0;
     	total += cc.get_solde();
     	total += ce.get_solde();
     	return total;
     }
-    public void remunerer() {ce.add_solde(ce.get_solde()*0.032);}
+    public void remunerer() {ce.add_solde(ce.get_solde()*0.032);} // méthode permettant de rémunerer le compte épargne.
 
 }
