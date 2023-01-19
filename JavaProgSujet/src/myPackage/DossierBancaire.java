3e706de771ae914d7a971553b3d2f2ba33a9ea3c
naism <naism@172.18.21.157>
2023-01-19 15:15:27 +0100

    Changement noms des variables DossierBancaire

--- JavaProgSujet/src/myPackage/DossierBancaire.java
+++ JavaProgSujet/src/myPackage/DossierBancaire.java
@@ -3,26 +3,26 @@ package myPackage;
 public class DossierBancaire {
 	
 	
-	private Compte_courant cc; // Création d'un objet compte courant .
-	private Compte_epargne ce; // Création d'un objet compte épargne.
+	private Compte_courant compte_c; // Création d'un objet compte courant .
+	private Compte_epargne compte_e; // Création d'un objet compte épargne.
 	public DossierBancaire() //Constructeur
     {
-    	cc = new Compte_courant(); //Initialisation des objets compte courant et compte épargne.
-    	ce = new Compte_epargne();
+		compte_c = new Compte_courant(); //Initialisation des objets compte courant et compte épargne.
+    	compte_e= new Compte_epargne();
     }
 
     public void deposer(double value) // méthode permettant de déposer de l'argent.
     {
-    	cc.add_solde(value*0.4);
-    	ce.add_solde(value*0.6);
+    	compte_c.add_solde(value*0.4);
+    	compte_e.add_solde(value*0.6);
     }
     public double get_solde() // méthode permettant de consulter le solde.
     {
     	double total = 0;
-    	total += cc.get_solde();
-    	total += ce.get_solde();
+    	total += compte_c.get_solde();
+    	total += compte_e.get_solde();
     	return total;
     }
-    public void remunerer() {ce.add_solde(ce.get_solde()*0.032);} // méthode permettant de rémunerer le compte épargne.
+    public void remunerer() {compte_e.add_solde(compte_e.get_solde()*0.032);} // méthode permettant de rémunerer le compte épargne.
 
 }
